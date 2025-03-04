db913b9c6e2d8004dea354f914b4deb6400818c9
hassan <hassan.boucherit@gmail.com>
2025-03-04 14:48:27 +0100

    feat : ajout de la varaible process.env.DBPORT pour les ports different de la bdd

--- backend/config/database.js
+++ backend/config/database.js
@@ -8,7 +8,8 @@ dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT || "mysql",
-  logging: false, 
+  logging: false,
+  port: parseInt(process.env.DB_PORT, 10) || 3306,
 });
 
 module.exports = sequelize;
