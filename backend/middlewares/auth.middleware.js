a984cd9e114bc5bc314cf861314e55cf9794752c
hassan <hassan.boucherit@gmail.com>
2025-03-04 14:42:54 +0100

    feat : ajout des middleware isWorker et isManager

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -3,14 +3,10 @@ const jwt = require("jsonwebtoken");
 
 const protect = (req, res, next) => {
     try {
-        
         const token = req.headers.authorization?.split(" ")[1];
-
         if (!token) {
             return res.status(401).json({ message: "Accès refusé. Token manquant" });
         }
-
-        
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
         req.user = decoded;
@@ -19,11 +15,26 @@ const protect = (req, res, next) => {
         res.status(401).json({ message: "Token invalide" });
     }
 };
+
+
 const isAdmin = (req, res, next) => {
     if (req.user.role !== 1) {
-        return res.status(403).json({ message: "Accès refusé. Seul un Responsable peut créer un utilisateur" });
+        return res.status(403).json({ message: "Accès refusé. Seul un Responsable peut acceder a ses options" });
     }
     next();
 };
 
-module.exports = { protect, isAdmin };
+const isWorker = (req, res, next) => {
+    if (req.user.role !== 2) {
+        return res.status(403).json({ message: "Accès refusé. Seul un Ouvrier peut acceder a ses options " });
+    }
+    next();
+}
+const isManager = (req, res, next) => {
+    if (req.user.role !== 3) {
+        return res.status(403).json({ message: "Accès refusé. Seul un Manager peut acceder a ses options " });
+    }
+    next();
+}
+
+module.exports = { protect, isAdmin , isWorker , isManager};
